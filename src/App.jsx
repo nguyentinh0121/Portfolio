28855e3ef2d38da120d65124a6ae92d898474eb7
LMS Developer <dev@lms.local>
2026-05-20 09:54:05 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+       
+      </section>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
