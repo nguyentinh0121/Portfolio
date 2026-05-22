205f617ce0a352efac59546c3c1a3cbd5cb76275
LMS Developer <dev@lms.local>
2026-05-22 21:56:08 +0700

    Page About and Contact

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,14 @@ function App() {
   return (
     <>
       <Header />
-      <Routes>
-        <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/product' element={<Product />} />
-        <Route path='/cv' element={<Cv />} />
-      </Routes>
+      <div className="app_container">
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/cv" element={<Cv />} />
+        </Routes>
+      </div>
     </>
   )
 }
--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import styles from './about.module.css'; 
+import avatar from '../../assets/images/avatar.jpg';
 
 export default function index() {
   return (
     <div className={styles.about}>
       <h1 className={styles.aboutTitle}>About<br/> Me</h1>
-      <img src="/avatar.jpg" alt="avatar" className={styles.avatar} />
+      <img src={avatar} alt='avatar' className={styles.avatar} />
+      <p className={styles.aboutName}>Nguyễn Đức Tính</p>
+      <p className={styles.aboutDescription}>
+        I am a passionate software developer with a strong background in web development. I have experience working with various technologies and frameworks, and I am always eager to learn and grow in my field. I enjoy creating efficient and user-friendly applications that solve real-world problems. In my free time, I like to explore new programming languages and contribute to open-source projects.
+      </p>
     </div>
   )
 }
